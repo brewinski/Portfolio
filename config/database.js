8dfb8df3453afa4e1852658ac286e1bb99543243
Christopher Brewin <Christopher Brewin>
2017-05-30 15:30:36 +1000

    Db Changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  database: "mongodb://cbrewin93@gmail.com:<Shadow93>@ds015700.mlab.com:15700/heroku_js4xtgh9",
+  database: "mongodb://cbrewin93@gmail.com:Shadow93@ds015700.mlab.com:15700/heroku_js4xtgh9",
   //database: "mongodb://localhost:27017/newmoon",
   secret: "shadow"
 }
