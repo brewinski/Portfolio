1109368ada47bf658bdc58495cf02147456fdeb8
Christopher Brewin <Christopher Brewin>
2017-06-08 12:41:52 +1000

    Updates to the angular web application UI styles look and feel

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -768,7 +768,7 @@ module.exports = "<nav class=\"navbar navbar-default navbar-fixed-top newmoon-na
 /***/ 691:
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"row newmoon-footer newmoon-row-opacity\">\n  <div class=\"col-md-6 col-md-offset-1\">\n  </div>\n  <div class=\"col-md-2 col-md-offset-3 newmoon-col\">\n    <i class=\"fa fa-facebook-square fa-2x\" aria-hidden=\"true\"></i>\n    <i class=\"fa fa-twitter-square fa-2x\" aria-hidden=\"true\"></i>\n    <i class=\"fa fa-instagram fa-2x\" aria-hidden=\"true\"></i>\n  </div>\n</div>\n"
+module.exports = "<div class=\"row newmoon-footer newmoon-row-opacity\">\n  <div class=\"col-md-6 col-md-offset-1\">\n  </div>\n  <div class=\"col-md-2 col-md-offset-3 newmoon-col\">\n    <a href=\"https://www.facebook.com/New-Moon-Web-Development-130227117553346/\" target=\"_blank\">\n      <i class=\"fa fa-facebook-square fa-2x\" aria-hidden=\"true\"></i>\n    </a>\n    <i class=\"fa fa-twitter-square fa-2x\" aria-hidden=\"true\"></i>\n    <i class=\"fa fa-instagram fa-2x\" aria-hidden=\"true\"></i>\n  </div>\n</div>\n"
 
 /***/ }),
 
